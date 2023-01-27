ddf25650b005165cdc3d0e78c5aabd705b27f24a
Werner Süß <werner.suess@itsv.at>
2023-01-27 12:53:10 +0100

    CHANGE: using vars for default values

--- mailer.go
+++ mailer.go
@@ -10,11 +10,22 @@ import (
     "github.com/fatih/color"
 )
 
+var SMTPD = "viruswall.sozvers.at"
+var SENDER = "rz.om.stp@itsv.at"
+
 func help() {
     color.Yellow("Usage: ")
-    fmt.Println("mailer [-f sender] [-t recipient,recipient] -s subject -b body/message [-a attachments] ")
-    fmt.Print("\nDefault sender and recipient is: ")
-    color.Cyan("rz.om.stp@itsv.at")
+    fmt.Println("mailer [-f sender] [-d] [-t recipient,recipient] -s subject -b body/message [-a attachments] ")
+    fmt.Println("\n -> use -h for more help!\n")
+    //fmt.Print("\nDefault sender and recipient is: ")
+    //color.Cyan("rz.om.stp@itsv.at")
+}
+
+func details() {
+  fmt.Print("Default sender: ")
+  color.Cyan(SENDER)
+  fmt.Print("Default SMTPD: ")
+  color.Cyan(SMTPD)
 }
 
 func mailer_single(from , to, subject, body , file string) {
@@ -24,7 +35,7 @@ func mailer_single(from , to, subject, body , file string) {
     m.SetHeader("Subject", subject)
     m.SetBody("text/plain", body)
 
-    d := gomail.Dialer{Host: "viruswall.sozvers.at", Port: 25}
+    d := gomail.Dialer{Host: SMTPD, Port: 25}
     if err := d.DialAndSend(m); err != nil {
         color.Red("\nWhoops, that didn't work, pal!")
         panic(err)
@@ -32,17 +43,23 @@ func mailer_single(from , to, subject, body , file string) {
 }
 
 func main() {
-    fromPart := flag.String("f", "rz.om.stp@itsv.at", "email-sender")
-    toPart := flag.String("t", "rz.om.stp@itsv.at", "email-recipient")
-    subjectPart := flag.String("s", "no subject", "email-subject")
-    bodyPart := flag.String("b", "(empty)", "email-body")
-    attachPart := flag.String("a", "none", "email-attachments")
+
+    showDetails := flag.Bool("d" , false, "Show default configuration settings." )
+
+    fromPart := flag.String("f", SENDER, "email-sender.")
+    toPart := flag.String("t", SENDER, "email-recipient.")
+    subjectPart := flag.String("s", "(no subject)", "email-subject.")
+    bodyPart := flag.String("b", "(empty)", "email-body.")
+    attachPart := flag.String("a", "(none)", "email-attachments.")
     flag.Parse()
+    if  *showDetails == true {
+      details()
+      os.Exit(0)
+    }
     if *subjectPart == "no subject" || *bodyPart == "(empty)" {
         //usage(5)
-        color.Red("I'm missing something, take a look at:")
+        color.Red("Sorry, I'm missing something.")
         help()
-        print("\n");
         os.Exit(1)
     } else {
         fmt.Println("Sender: \t", *fromPart)
