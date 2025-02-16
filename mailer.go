a211b3257faeaf90545b72b69b52e2193d9b8c5b
sueswe <nookie.suess@gmail.com>
2025-02-16 16:29:06 +0100

    EXTEND: logging for info and error (stdout and stderr)

--- mailer.go
+++ mailer.go
@@ -16,7 +16,7 @@ import (
 TODO: more attachments comma-seperated.
 */
 
-var version string = "0.3.3"
+var version string = "0.3.4"
 
 var SMTPD string
 var SENDER string
@@ -30,17 +30,21 @@ func help() {
 
 func main() {
 
-	log.Print("mailer, Version ", version)
+	infoLog := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	//warningLog := log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog := log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	infoLog.Print("mailer, Version ", version)
 
 	config, err := toml.LoadFile(home + "/mailerconfig.toml")
 	if err != nil {
-		fmt.Println("Error ", err.Error())
+		errorLog.Print("Error ", err.Error())
 		panic(err)
 	}
 	SMTPD := config.Get("default.SMTPD").(string)
 	SENDER := config.Get("default.SENDER").(string)
-	log.Print("Mailserver: ", SMTPD)
-	log.Print("Sender: ", SENDER)
+	infoLog.Print("Mailserver: ", SMTPD)
+	infoLog.Print("Sender: ", SENDER)
 
 	//showConfig := flag.Bool("c", false, "Show default configuration settings from configfile.")
 	fromPart := flag.String("f", SENDER, "email-sender.")
@@ -56,14 +60,14 @@ func main() {
 	//}
 
 	if *subjectPart == "(no subject)" || *bodyPart == "(empty)" {
-		log.Print("Sorry, I'm missing a mandatory parameter.")
+		errorLog.Print("Sorry, I'm missing a mandatory parameter.")
 		help()
 		os.Exit(2)
 	} else {
 		//log.Print("Sender: \t", *fromPart)
-		log.Print("Recipient: \t", *toPart)
-		log.Print("Subject: \t", *subjectPart)
-		log.Print("Body: \t", *bodyPart)
+		infoLog.Print("Recipient: \t", *toPart)
+		infoLog.Print("Subject: \t", *subjectPart)
+		infoLog.Print("Body: \t", *bodyPart)
 	}
 
 	m := gomail.NewMessage()
@@ -82,16 +86,16 @@ func main() {
 
 	// Attachment-Parameter verarbeiten:
 	if *attachPart == "(none)" {
-		log.Print("Attachment:\t", *attachPart)
+		infoLog.Print("Attachment:\t", *attachPart)
 	} else {
 		//log.Print("globbing attachments: ", *attachPart)
 		filenames, err := filepath.Glob(*attachPart)
 		if err != nil {
-			log.Print("glob-error")
+			errorLog.Print("glob-error")
 			os.Exit(2)
 		}
 		if len(filenames) == 0 {
-			log.Print("Attachments-Glob ist leer.")
+			errorLog.Print("Attachments-Glob ist leer.")
 			os.Exit(3)
 		}
 		for i, fname := range filenames {
@@ -99,21 +103,21 @@ func main() {
 			_, error := os.Stat(fname)
 			// check if error is "file not exists"
 			if os.IsNotExist(error) {
-				log.Print("file does not exist: ", fname)
+				errorLog.Print("file does not exist: ", fname)
 				os.Exit(5)
 			}
-			log.Print("Attaching file ", i, " ,is: ", fname)
+			infoLog.Print("Attaching file ", i, " ,is: ", fname)
 			m.Attach(fname)
 		}
 	}
 
-	log.Print("Trying to send ...")
+	infoLog.Print("Trying to send ...")
 	d := gomail.Dialer{Host: SMTPD, Port: 25}
 	if err := d.DialAndSend(m); err != nil {
-		log.Print("Sorry, that didn't work.")
+		errorLog.Print("Sorry, that didn't work.")
 		panic(err)
 	} else {
-		log.Print("done.")
+		infoLog.Print("done.")
 	}
 
 } //main_end
